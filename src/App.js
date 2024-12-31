b426b55bef38e20d58e82648625745cf7f3a7893
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2024-12-31 14:25:11 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -30,9 +30,9 @@ function Board() {
   const winner = calculateWinner(squares);
   let status;
   if (winner)
-    status = "Winner: " + winner;
+    status = "Winner : " + winner;
   else
-    status = "Next player: " + (xIsNext ? "X" : "O");
+    status = "Next player : " + (xIsNext ? "X" : "O");
   return (
     <>
       <div className='status'>{status}</div>
